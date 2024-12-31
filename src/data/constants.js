c06b56f3c7135472393b527f1c0868e4e8fe9d67
ARAVIND OSURI <aravindosuri2000@gmail.com>
2024-12-31 13:15:45 -0600

    add

---
+++ src/data/constants.js
@@ -0,0 +1,331 @@
+import { Stack } from "@mui/material";
+
+export const Bio = {
+  name: "ARAVIND KUMAR",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    
+"I'm a highly motivated and versatile Full Stack Developer with 3 years of experience,committed to continuous development. I thrive in tackling complex challenges, analyzing data, and building efficient, scalable solutions across both front-end and back-end technologies. A firm believer in lifelong learning, I actively seek opportunities to expand my knowledge and refine my skillset in the ever-evolving tech landscape.",
+  github: "https://github.com/Aravind-011",
+  gmail: "avi996631@gmail.com",
+  resume:
+    "https://drive.google.com/file/d/1SBh57_tvGZvHHtKwIFOKwAIWn9Vuvv6A/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/kumar-avinath1702/",
+
+};
+
+export const skills = [
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/C_Logo.png/640px-C_Logo.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/640px-ISO_C%2B%2B_Logo.svg.png",
+      },
+     
+      {
+        name: "Java",
+        image: "https://www.oracle.com/img/tech/cb88-java-logo-001.jpg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/640px-Python-logo-notext.svg.png"      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "C#",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/C_Sharp_Icon.png/640px-C_Sharp_Icon.png",
+      },
+      // {
+      //   name: "",
+      //   image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/768px-Tailwind_CSS_Logo.svg.png?20230715030042",
+      // },
+      {
+        name: "DSA",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLlESc6Idq0yeZQQjfcYSXIPaa7jkumHWTyA&s",
+      },
+   
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      // {
+      //   name: "Redux",
+      //   image:
+      //     "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      // },
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/768px-Tailwind_CSS_Logo.svg.png?20230715030042",
+      },
+      // {
+      //   name: "Shadcn UI",
+      //   image:
+      //     "https://raw.githubusercontent.com/anshita005/My-Portfolio/main/shadcn-ui-logo-png_seeklogo-519786.png",
+      // },
+   
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name:"JSP",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ4ODPCEYDxGvbggpZvfpzaMJvskiOa-RHlw&s",
+      },
+
+      {
+        name: "Spring",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGirL9yUSpxb7hEa7JlHiwsI8WqE2fMHmUlw&s",
+      },
+      {
+        name: "SpringBoot",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Springboot.jpg/640px-Springboot.jpg",},
+      {
+        name:"Rest API",
+        image:"https://www.opc-router.de/wp-content/uploads/2024/02/REST-800-440-en.png",
+      },
+   
+      {
+        name: "MSSQL",
+        image:"https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "Oracle Databse",
+        image:"https://play-lh.googleusercontent.com/lvUW3XukTSHnop1wGHVO1cn2EMwoEa2ZfjBhpIt0t1MhjWXa319OUHJ4zzzhdzf6Vis=w240-h480-rw",
+      },
+      // {
+      //   name: "Supabase",
+      //   image: "https://raw.githubusercontent.com/anshita005/My-Portfolio/main/supabase-logo-DCC676FFE2-seeklogo.com.png",
+      // },
+    ],
+  },
+
+  {
+    title:"Tools",
+    skills: [
+      {
+        name: "Docker",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRS07f6A9NY_5K5Ie-s-P4M1ErApMYC_t9fg&s",},
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Jenkins_logo.svg/1483px-Jenkins_logo.svg.png",
+      },
+      {
+        name:"Terraform",
+        image:"https://www.aviator.co/blog/wp-content/uploads/2023/01/terraform.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+
+    ],
+  },
+
+  {
+    title:"MERN Stack",
+    skills:[
+    {
+    name:"NodeJs",
+    image:"https://miro.medium.com/v2/resize:fit:800/1*bc9pmTiyKR0WNPka2w3e0Q.png",
+    },
+    {
+      name:"ExpressJS",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr3Sp_9UnvsXgAfScS9AHtLrnKfSkAo5RLmw&s",
+    },
+    {
+       name:"MogoDb",
+       image:"https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png"
+    },
+     {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+    ]
+  }
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C4E0BAQEK2_o9ZrGO1Q/company-logo_200_200/company-logo_200_200/0/1644381899191/informationdatasystems_logo?e=2147483647&v=beta&t=OFsuZHWsVscAp6T2uG4eW2cl2mwbIZE8A2MvZ9W7jxk",
+    role: "Software Engineer ",
+    company: " Information Data Systems [USA]",
+    date: "Jan 2024 - Present",
+    desc: "Aligned with over 15 business stakeholders and project leads to define critical project requirements, streamlining processes and reducing project delivery timelines by 20%, which enhanced team productivity and stakeholder satisfaction. Engineered and deployed a personalized customer interaction module for a CRM system, boosting customer engagement by 15% through improved user experience and efficient data processing. Achieved a 20% reduction in page load times for a web application, significantly enhancing user satisfaction. Led a team to refactor legacy code, resulting in a 30% reduction in user-reported bugs and a 15% increase in code maintainability.",
+    // skills: [
+    //   "ReactJS",
+    //   "Redux",
+    //   "Java",
+    //   "SpringBoot",
+    //   "JavaScript",
+    //   "API Integration",
+    //   "Oracle",
+    // ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  },
+  {
+    id: 5,
+    img: "https://www.ulsystem.edu/wp-content/uploads/2019/02/ULL.png",
+    role: "University Of Louisiana at Lafayette ",
+    company: "Graduate Research Assistant",
+    date: "Jan 2023 - Dec 2023",
+    desc: "As a Graduate Research Assistant, I contributed to several impactful projects, including implementing the user interface for a step clock application, where I focused on designing an intuitive and user-friendly interface to enhance user experience. Additionally, I assisted in Python-based machine learning projects, handling tasks such as data preprocessing, feature selection, and model development. I was also involved in managing and optimizing databases, ensuring data integrity, and facilitating seamless integration between machine learning algorithms and the underlying database.",
+  },
+  {
+    id: 5,
+    img: "https://yt3.googleusercontent.com/aXWliQ1f4jgCooGyiA-7uhyz3WbdUY1eplOhQ9UhiYLhukCkES6aqcKZtSm4Nc-NVEwwLGmV=s900-c-k-c0x00ffffff-no-rj",
+    role: "HCL Technologies ",
+    company: "Java full Stack Developer",
+    date: "June 2020 - Dec 2022",
+    desc: "I revitalized user interfaces using React.js, optimizing performance and reducing the bounce rate to 15%, which significantly improved the overall user experience. I developed scalable backend applications with Next.js, leveraging server-side rendering and API routes to enhance functionality and user engagement. Collaborating closely with UI/UX designers, I contributed to prototypes and wireframes for six web applications, gathering actionable insights from feedback sessions with over 150 participants to refine products. By employing code refactoring, caching strategies, and performance profiling, I achieved a 55% improvement in application load times. I also implemented a robust, multi-layered security framework with advanced encryption and user authentication, benefiting over 50 employees and bolstering data security. Additionally, I organized training sessions for 20+ team members, fostering collaboration and enhancing productivity throughout the software development lifecycle.",
+  },
+
+  {
+    id: 5,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0333ohUDsvMbXzM6QJNc3zhUnL4oc81Q-GA&s",
+    role: "Pantech Solutions ",
+    company: " Java Developer Intern",
+    date: "June 2019 - Dec 2019",
+    desc: "As a Java Developer Intern, I collaborated with senior developers to design and implement Java-based solutions, adhering to coding standards and best practices. I contributed to the development and testing of core backend functionality, optimizing performance and enhancing features. I assisted in creating RESTful APIs to facilitate communication between the front-end and back-end of web applications and participated in bug fixes and code refactoring, improving code efficiency.",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.ulsystem.edu/wp-content/uploads/2019/02/ULL.png",
+    school: "University Of Louisiana at Lafayette",
+    grade: " 3.7 CGPA",
+    desc: "I hold a Master’s degree in Computer Science with a CGPA of 3.7. My academic journey included coursework in foundational and advanced subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Master of Science - MS, Computer Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Ecommerce Full Stack App",
+    description:
+      "Built a Job Portal app with React.js, allowing candidates to apply, save jobs, and recruiters to manage listings, with secure authentication via Clerk. Integrated Supabase for real-time data updates and used Redux Toolkit for state management. Designed a responsive, modern UI with ShadCN and Tailwind CSS for cross-device compatibility.",
+    image:
+      "https://www.export.org.uk/media/cvb9zpgu/ECommerce.jpg",
+    tags: [
+      "React Js",
+      "Shadcn UI",
+      "Supabase",
+      "Tailwind CSS",
+      "Javascript",
+      "Clerk",
+    ],
+    category: "web app",
+    github: "https://github.com/Aravind-011/Ecommerce-Web-App.git",
+    webapp: "https://github.com/Aravind-011",
+  },
+  {
+    id: 12,
+    title: "My Portfolio",
+    description:
+      "My Portfolio is a React.js application that serves as an online portfolio to showcase my work, skills, and achievements. It provides an overview of my background, displays my projects, and offers a way to contact me.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1400/1*IGda1f7np5KIup0lMHBjWg.jpeg",
+    tags: [
+      "React Js",
+      "HTML5",
+      "Tailwind CSS",
+      "Javascript",
+     
+    ],
+    category: "web app",
+    github: "https://github.com/anshita005/My-Portfolio",
+    webapp: "https://github.com/anshita005",
+  },
+  {
+    id: 9,
+    title: " Asian Restaurant UI/UX",
+    description:
+      " SpendWise is a web application designed to streamline expense management and enhance financial tracking system with real-time logging, secure authentication via Express.js, and MongoDB for managing expenses. Designed a responsive UI using Ant Design, ensuring seamless cross-device budgeting and tracking.",
+    image:
+      "https://media.gettyimages.com/id/1388791738/photo/teppanyaki-style.jpg?s=2048x2048&w=gi&k=20&c=EZ-RgtVIecO9ozNOtuUCNNTWBOOfDxXtzsUMmZ2J-Dc=",
+    tags: [
+      "ReactJs",
+      "Javascript",
+      "HTML",
+      "CSS",
+      "BootStrap", 
+    ],
+    category: "web app",
+    github: "https://github.com/Aravind-011/UI-UX.git",
+    webapp: "https://github.com/anshita005/SpendWise",
+  },
+  {
+    id: 0,
+    title: "Student Placement Prediction Using ML",
+    description:
+      "Developed a responsive Gemini platform clone using the Gemini API for real-time data processing, ensuring seamless user interaction. Leveraged React Hooks and Context API for efficient state management and a smoother user experience.",
+    image:
+      "https://adarshvodnala.vercel.app/images/studentPlacement.png",
+    tags: ["React Js", "Javascript", "GeminiAPI", "CSS3"],
+    category: "web app",
+    github: "https://github.com/anshita005/Gemini-Clone-Project",
+    webapp: "https://github.com/anshita005/Gemini-Clone-Project",
+  },
+  
+];
+
